1ea8c0bb5ce91447ee88cb111d87da732d7de6c0
James Allen <jacques2186@yahoo.com>
2026-02-07 14:34:23 -0500

    Add jump to blogs link

--- resources/views/components/⚡categories.blade.php
+++ resources/views/components/⚡categories.blade.php
@@ -17,11 +17,14 @@ new class extends Component
 
 <div class="bg-tan-light py-16">
     <h2 class="text-3xl text-center font-serif mb-8">Categories</h2>
+    <p class="text-center mb-4"><a href="#blogs">(Jump to blogs)</a></p>
     <div class="max-w-6xl mx-auto grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-8 px-8">
         @foreach($this->getCategories as $category)
             <div class="text-center bg-white shadow-lg">
                 <div class="aspect-square overflow-hidden">
-                    <img class="w-full h-full object-cover" src="{{ config('app.base_api_endpoint') . data_get($category, 'imgSrc')}}" alt="" />
+                    <a href="{{ Uri::route('post.index', ['categoryName' => data_get($category, 'categorySlug')])->withFragment('blogs') }}">
+                        <img class="w-full h-full object-cover" src="{{ config('app.base_api_endpoint') . data_get($category, 'imgSrc')}}" alt="" />
+                    </a>
                 </div>
                 <h3 class="text-xl p-4 font-bold"><a href="{{ Uri::route('post.index', ['categoryName' => data_get($category, 'categorySlug')])->withFragment('blogs') }}">{{ data_get($category, 'title') }}</a></h3>
             </div>
