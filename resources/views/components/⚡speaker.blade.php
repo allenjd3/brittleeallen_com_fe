a9b39b46c151043f097fe529cd6361587d9e3abe
James Allen <jacques2186@yahoo.com>
2026-02-23 21:22:18 -0500

    Fix header

--- resources/views/components/⚡speaker.blade.php
+++ resources/views/components/⚡speaker.blade.php
@@ -10,7 +10,7 @@ new class extends Component
 
 <div class="max-w-5xl mb-32 mx-auto px-4">
     <img class="w-full max-w-3xl mx-auto" src="{{ asset('/images/britt_speaking.webp') }}" alt="Brittany Allen" />
-    <h1 class="text-4xl my-8 font-serif text-center capitalize">Looking for a speaker for your next Women’s event?</h1>
+    <h1 class="text-4xl my-8 font-serif text-center capitalize text-pretty max-w-3xl mx-auto">Looking for a speaker for your next Women’s event?</h1>
     <div class="prose max-w-full prose-lg text-pretty">
         <p>Brittany is available for weekend retreats or conferences. If you’re looking to book her for your next women’s event, please fill out this form.</p>
         <p>Choose from the following topics or inquire about your own:</p>
